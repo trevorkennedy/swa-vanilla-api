a2a3e724d671941e4b1c01a336fcfcd01328abdd
trevorkennedy <direct@trevorkennedy.com>
2020-06-27 16:50:29 -0500

    api

---
+++ api/GetMessage/index.js
@@ -0,0 +1,47 @@
+const fs = require('fs');
+const util = require('util');
+const readFileAsync = util.promisify(fs.readFile);
+
+async function readFile(path) {
+  // Recommended file read pattern
+  let data;
+  try {
+      data = await readFileAsync(path);
+  } catch (err) {
+      context.log.error('ERROR', err);
+      throw err;
+  }
+  return data;
+}
+
+function buildBody(body) {
+  var message = {
+    content: [{
+        type: 'text/plain',
+        value: body
+    }]
+  };
+
+  return message;
+}
+
+module.exports = async function (context, req) {
+  var msg = 'JavaScript HTTP trigger function request\n';
+  msg += `Time: ${new Date().toISOString()}\n`;
+  msg += `Runtime: ${process.env['FUNCTIONS_WORKER_RUNTIME']}\n`;
+  msg += `Time Zone: ${process.env['WEBSITE_TIME_ZONE']}\n`;
+  msg += `Site Name: ${process.env['WEBSITE_SITE_NAME']}\n`;
+  msg += `SKU: ${process.env['WEBSITE_SKU']}\n`;
+  msg += `Compute Mode: ${process.env['WEBSITE_COMPUTE_MODE']}\n`;
+  msg += `Hostname: ${process.env.WEBSITE_HOSTNAME}\n`;
+  msg += `Environment: ${process.env['AZURE_FUNCTIONS_ENVIRONMENT']}\n`;
+  msg += `Custom Value: ${process.env['CUSTOM_VALUE']}\n`;
+  msg += `Request Body: ${req.body}\n`;
+  msg += `Query Param: ${req.query.redirectUrl}\n`;
+  msg += `Contect biding: ${context.bindingData.id}\n`;
+  msg += `File Contents: ${await readFile('./hello.txt')}\n`;
+  
+  context.log(msg);
+  //context.bindings.sendGrid = buildBody(msg);
+  context.res = {body: {text: msg}};
+};
--- readme.md
+++ readme.md
@@ -1,3 +1,12 @@
 # Vanilla JavaScript App
 
 This repo is used as a starter for a _very basic_ HTML web application using no front-end frameworks.
+
+Example: http://localhost:7071/api/message/1?redirectUrl=blah
+
+az functionapp config appsettings list --name <FUNCTION_APP_NAME> \
+--resource-group <RESOURCE_GROUP_NAME>
+
+az functionapp config appsettings set --name <FUNCTION_APP_NAME> \
+--resource-group <RESOURCE_GROUP_NAME> \
+--settings CUSTOM_FUNCTION_APP_SETTING=12345
